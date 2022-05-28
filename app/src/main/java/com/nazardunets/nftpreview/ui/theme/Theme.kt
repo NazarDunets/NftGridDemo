9bb9915e752e9fb098ea0a8f8eadbc3767ba6701
Nazar Dunets <nazar.dunets@pm.bet>
2022-05-28 19:11:35 +0300

    update color theme

--- app/src/main/java/com/nazardunets/nftpreview/ui/theme/Theme.kt
+++ app/src/main/java/com/nazardunets/nftpreview/ui/theme/Theme.kt
@@ -1,30 +1,18 @@
 package com.nazardunets.nftpreview.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
+import androidx.compose.ui.graphics.Color
 
 @Composable
-fun NFTPreviewTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+fun NFTPreviewTheme(content: @Composable () -> Unit) {
+    val colors = darkColors(
+        primary = Purple200,
+        primaryVariant = Purple700,
+        secondary = Teal200,
+        background = Color.Black
+    )
 
     MaterialTheme(
         colors = colors,
